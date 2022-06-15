24898d2cb59ab6ac7cb87c7202305e7126da5d73
Michael Justice <michaeljusticework@gmail.com>
2022-06-14 19:08:13 -0700

    Changes -Added helloworld1.py

---
+++ helloworld1.py
@@ -0,0 +1,10 @@
+# This program will say hello, than ask for your name. 
+print('Hello World')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print ('The length of your name is:')
+print(len(myName))
+print('What is your age?') #asks for your age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
